bcf93f62cb900f47badbf5bb678f8f8aac2ee159
PARIAnandha <parianandha@gmail.com>
2024-10-23 00:59:11 +0530

    resume update1

--- React-Portfolio/src/components/About.jsx
+++ React-Portfolio/src/components/About.jsx
@@ -1,16 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import aboutImg from '../assets/hero.jpg';
 
 const About = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
+  const info = [
+    { text: "Years experience", count: "04" },
+    { text: "Completed Projects", count: "24" },
+    { text: "Companies Work", count: "06" },
+  ];
 
   return (
     <section id="about" className="py-10 text-white">
@@ -23,52 +19,26 @@ const About = () => {
           <div className="p-2">
             <div className="text-gray-300 my-3">
               <p className="text-justify leading-7 w-11/12 mx-auto">
-                Hello, I'm Pari from Peramballur, currently based in Coimbatore. I am a Full Stack Developer with one year of experience working at Zeeraa Private Limited Company. I have a solid background in web development and am passionate about creating efficient and user-friendly applications.
+                Hello, I'm Pari from Peramballur, currently based in Coimbatore. I am a Full Stack Developer with one year of experience working at Zeeraa Private Limited Company. I have a solid background in web development and am passionate about creating efficient and user-friendly applications. My approach combines technical proficiency with a strong emphasis on teamwork and communication, allowing me to effectively contribute to projects of various scales. I am dedicated to continuous improvement and stay updated with the latest trends and technologies in the industry to ensure my skills remain sharp.
               </p>
               <br />
               <br />
-              <button onClick={openModal} className="btn-primary">
-                Download CV
-              </button>
+              <a href="/React-Portfolio/public/PARI-ARUL Resume.pdf" download="PARI-ARUL Resume.pdf">
+                <button className="btn-primary">Download CV</button>
+              </a>
             </div>
           </div>
           <div className="flex-1 md:mt-0 mt-6 flex justify-center items-center">
-            <div className="lg:w-96 h-full relative sm:w-10/12 w-11/12 max-w-sm aboutImg">
+            <div className="lg:w-96 h-full relative sm:w-10/12 w-11/12 max-w-sm aboutImg ">
               <img
                 src={aboutImg}
-                alt="About Me"
+                alt=""
                 className="w-full object-cover bg-cyan-600 rounded-xl"
               />
             </div>
           </div>
         </div>
       </div>
-
-      {/* Modal */}
-      {isModalOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-          <div className="bg-white p-6 rounded-lg text-center">
-            <h2 className="text-2xl font-bold mb-4">Download Resume</h2>
-            <p className="text-gray-600 mb-6">
-              Click the button below to download my resume.
-            </p>
-            <a
-              href="/PARI-ARUL Resume.pdf"
-              download="PARI-ARUL Resume.pdf"
-              className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300"
-            >
-              Download PDF
-            </a>
-            <br />
-            <button
-              onClick={closeModal}
-              className="mt-4 bg-gray-500 text-white px-4 py-2 rounded-md hover:bg-gray-600 transition duration-300"
-            >
-              Close
-            </button>
-          </div>
-        </div>
-      )}
     </section>
   );
 };
